cd354a10de714e771e64fda3fba6921cd3883479
i8lex <cybeerid@gmail.com>
2022-10-24 22:39:11 +0300

    little upgrade, code shortening

--- index.js
+++ index.js
@@ -2,80 +2,30 @@ const body = document.querySelector(`body`);
 body.innerHTML =
     `<section class="section">
        <div class="container">
-           <ul class="section__wrapper">
-               
-           </ul>
+           <ul class="section__wrapper"></ul>
        </div>
     </section>`
 
-
 /**
  *
- * @param {data[]} arrData
- *
- * @param {{name, photo, position}} arrData
+ * @param {string} name
+ * @param {string} photo
+ * @param {string} position
  */
 
-function renderData(arrData) {
+function renderData({ name, photo, position }) {
     const wrapper = document.querySelector(`.section__wrapper`);
-    wrapper.innerHTML = ``;
+    // wrapper.innerHTML = ``;
 
-
-    arrData.forEach((user) => {
-
-        const list = document.createElement(`li`);
-        list.classList.add(`section__list`);
-        wrapper.append(list)
-
-        const listName = document.createElement(`h6`);
-        listName.className = `section__list__name`;
-        listName.innerText = user.name
-        list.append(listName)
-
-        const listPhoto = document.createElement(`img`);
-        listPhoto.className = `section__list__photo`;
-        listPhoto.src = user.photo
-        listPhoto.alt = user.name
-        list.append(listPhoto)
-
-        const listPosition = document.createElement(`p`);
-        listPosition.className = `section__list__position`;
-        listPosition.innerText = user.position;
-        list.append(listPosition);
-    })
-
-
-    // arrData.forEach((user) => renderUser(user));
-
-    // /**
-    //  *
-    //  * @param {{name, photo, position}} userData
-    //  */
-
-    // function renderUser(userData) {
-    //
-    //     const list = document.createElement(`li`);
-    //     list.classList.add(`section__list`);
-    //     wrapper.append(list)
-    //
-    //     const listName = document.createElement(`h6`);
-    //     listName.className = `section__list__name`;
-    //     listName.innerText = userData.name
-    //     list.append(listName)
-    //
-    //     const listPhoto = document.createElement(`img`);
-    //     listPhoto.className = `section__list__photo`;
-    //     listPhoto.src = userData.photo
-    //     listPhoto.alt = userData.name
-    //     list.append(listPhoto)
-    //
-    //     const listPosition = document.createElement(`p`);
-    //     listPosition.className = `section__list__position`;
-    //     listPosition.innerText = userData.position;
-    //     list.append(listPosition);
-    // }
+    const list = document.createElement(`li`);
+    list.classList.add(`section__list`);
+    list.innerHTML = `
+        <h6 class="section__list__name">${name}</h6>
+        <img class="section__list__photo" src="${photo}" alt="${name}">
+        <p class="section__list__position">${position}</p>
+    `
+    wrapper.append(list)
 }
-
 
 /**
  *
@@ -85,7 +35,7 @@ function renderData(arrData) {
  * @returns {Promise<string>}
  */
 
-function fetch (url, { method = `GET`, body = null } = {} ) {
+function customFetch (url, { method = `GET`, body = null } = {} ) {
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
 
@@ -102,10 +52,10 @@ function fetch (url, { method = `GET`, body = null } = {} ) {
     });
 }
 
-fetch(`https://users-api-id.herokuapp.com/users`)
+customFetch(`https://users-api-id.herokuapp.com/users`)
     .then((result) =>{
     const data = JSON.parse(result)
         console.log(data)
-        renderData(data);
+        data.forEach(renderData);
 })
 
